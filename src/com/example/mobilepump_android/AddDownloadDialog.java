f188ad635a83a67206ff3f5128af9d6100129029
lapshin <Anton_Evgenich@mail.ru>
2013-10-08 22:23:09 +0400

    Обрабтчики нажатия кнопок в диалоге добавления новой загрузки теперь в вызывающей Activity

--- src/com/example/mobilepump_android/AddDownloadDialog.java
+++ src/com/example/mobilepump_android/AddDownloadDialog.java
@@ -1,12 +1,9 @@
 package com.example.mobilepump_android;
 
-import android.app.AlertDialog;
-import android.app.Dialog;
-import android.app.DialogFragment;
+import android.app.*;
 import android.content.DialogInterface;
 import android.os.Bundle;
 import android.view.LayoutInflater;
-import android.widget.Toast;
 
 /**
  * Created with IntelliJ IDEA.
@@ -16,6 +13,32 @@ import android.widget.Toast;
  * To change this template use File | Settings | File Templates.
  */
 public class AddDownloadDialog extends DialogFragment {
+
+    public interface AddDownloadDialogListener {
+        public void onDialogPositiveClick(DialogFragment dialog);
+        public void onDialogNegativeClick(DialogFragment dialog);
+    }
+
+    AddDownloadDialogListener mListener;
+
+    @Override
+    public void onAttach(Activity activity) {
+        super.onAttach(activity);
+        // Verify that the host activity implements the callback interface
+        try {
+            mListener = (AddDownloadDialogListener) activity;
+        } catch (ClassCastException e) {
+            // The activity doesn't implement the interface, throw exception
+            throw new ClassCastException(activity.toString()
+                    + " must implement NoticeDialogListener");
+        }
+    }
+
+
+    public static void show(FragmentManager fragmentManager){
+        AddDownloadDialog dialog = new AddDownloadDialog();
+        dialog.show(fragmentManager, "add_download_dialog");
+    }
 
     @Override
     public Dialog onCreateDialog(Bundle savedInstanceState) {
@@ -27,12 +50,12 @@ public class AddDownloadDialog extends DialogFragment {
                 .setPositiveButton(R.string.ok, new DialogInterface.OnClickListener() {
                     @Override
                     public void onClick(DialogInterface dialog, int id) {
-                        Toast.makeText(getActivity(), "Add download", Toast.LENGTH_SHORT).show();
+                        mListener.onDialogPositiveClick(AddDownloadDialog.this);
                     }
                 })
                 .setNegativeButton(R.string.cancel, new DialogInterface.OnClickListener() {
                     public void onClick(DialogInterface dialog, int id) {
-                        AddDownloadDialog.this.getDialog().cancel();
+                        mListener.onDialogNegativeClick(AddDownloadDialog.this);
                     }
                 });
         return builder.create();
--- src/com/example/mobilepump_android/MainActivity.java
+++ src/com/example/mobilepump_android/MainActivity.java
@@ -2,17 +2,26 @@ package com.example.mobilepump_android;
 
 import android.app.Activity;
 import android.app.DialogFragment;
+import android.app.DownloadManager;
+import android.content.Context;
 import android.os.Bundle;
 import android.view.Menu;
 import android.view.MenuItem;
 import android.widget.SearchView;
+import android.widget.Toast;
 
-public class MainActivity extends Activity {
+public class MainActivity extends Activity implements AddDownloadDialog.AddDownloadDialogListener {
+
+    private DownloadManager mDownloadManager;
+
+    private boolean isOnlyWiFi = true;
 
     @Override
     public void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.main);
+
+        mDownloadManager = (DownloadManager) getSystemService(Context.DOWNLOAD_SERVICE);
     }
 
     @Override
@@ -26,8 +35,7 @@ public class MainActivity extends Activity {
     public boolean onOptionsItemSelected(MenuItem item) {
         switch (item.getItemId()) {
             case R.id.add_download: {
-                DialogFragment dialog = new AddDownloadDialog();
-                dialog.show(getFragmentManager(), "add_download_dialog");
+                AddDownloadDialog.show(getFragmentManager());
                 return true;
             }
             default:
@@ -35,4 +43,13 @@ public class MainActivity extends Activity {
         }
     }
 
+    @Override
+    public void onDialogPositiveClick(DialogFragment dialog) {
+        Toast.makeText(this, "Add download", Toast.LENGTH_SHORT).show();
+    }
+
+    @Override
+    public void onDialogNegativeClick(DialogFragment dialog) {
+        dialog.getDialog().cancel();
+    }
 }
